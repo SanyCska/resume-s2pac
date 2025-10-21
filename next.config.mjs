1a8d8e7e9dc1a703c9f91063e23dcc770f8c56f9
a-s2pac <aleksandr.stupak@offsidegaming.com>
2025-10-21 20:46:24 +0200

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'dist',
-  basePath: process.env.NODE_ENV === 'production' ? '/resume-s2pac' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/resume-s2pac' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
